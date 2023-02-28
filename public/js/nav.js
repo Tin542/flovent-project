c6dea049371a6ecc7c90eb1c2520f1ac53eba6b6
TinNT <tin123456bim@gmail.com>
2023-03-01 01:30:08 +0700

    fix navbar

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,35 +1,36 @@
 const createNav = () => {
-  let nav = document.querySelector(".navbar");
+  let nav = document.querySelector(".nav-bar");
   nav.innerHTML = `
-    <div class="nav">
-    <img src="image/logo.png" class="brand-logo" alt="" />
-    <div class="nav-items">
-      <div class="search">
-        <ul class="link-container">
-          <li class="link-items">
-            <a href="#" class="link">Trang chủ</a>
-          </li>
-          <li class="link-items"><a href="#" class="link">Sản phẩm</a></li>
-          <li class="link-items"><a href="#" class="link">Dịch vụ</a></li>
-          <li class="link-items"><a href="#" class="link">Blog</a></li>
-        </ul>
+  <div class="nav">
+        <img src="image/logo.png" class="brand-logo" alt="" />
+        <div class="nav-items">
+          <div class="search">
+            <ul class="link-container">
+              <li class="link-items">
+                <a href="#" class="link">Trang chủ</a>
+              </li>
+              <li class="link-items">
+                <a href="#" class="link">Sản phẩm</a>
+              </li>
+              <li class="link-items"><a href="#" class="link">Dịch vụ</a></li>
+              <li class="link-items"><a href="#" class="link">Blog</a></li>
+            </ul>
+          </div>
+          <a href="#" class="nav-icon"
+            ><img style="width: 20px" src="image/user.png" alt=""
+          /></a>
+          <a href="#" class="nav-icon"
+            ><img style="width: 25px" src="image/cart.png" alt=""
+          /></a>
+          <a href="#" class="nav-icon"
+            ><img style="width: 35px" src="image/delivery.png" alt=""
+          /></a>
+          <div class="btn-login-container">
+            <button class="btn-login">Đăng nhập</button>
+          </div>
+        </div>
       </div>
-      <a href="#"
-        ><img style="width: 20px" src="image/user.png" alt=""
-      /></a>
-      <a href="#"
-        ><img style="width: 25px" src="image/cart.png" alt=""
-      /></a>
-      <a href="#"
-        ><img style="width: 35px" src="image/delivery.png" alt=""
-      /></a>
-      <div class="btn-login-container">
-        <button class="btn-login">Đăng nhập</button>
-      </div>
-    </div>
-  </div>
        `;
 };
-
 
 createNav();
