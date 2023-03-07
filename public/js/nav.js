1ad7cb268fbc56aac93c6898153ba174643a8a01
TinNT <tin123456bim@gmail.com>
2023-03-07 14:48:52 +0700

    add cart page

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,8 +1,16 @@
 const createNav = () => {
   let nav = document.querySelector(".nav-bar");
-  
+
   nav.innerHTML = `
-  <div class="nav" id="myTopnav">
+  <div class="nav">
+        <div class="nav-responsive">
+          <button onclick="dropdownbtn()" class="dropbtn">
+            <i
+              class="fa fa-align-justify"
+              style="font-size: 50px; color: black"
+            ></i>
+          </button>
+        </div>
         <img src="image/logo.png" class="brand-logo" alt="" />
         <div class="nav-items">
           <div class="search">
@@ -20,7 +28,7 @@ const createNav = () => {
           <a href="#" class="nav-icon"
             ><img style="width: 20px" src="image/user.png" alt=""
           /></a>
-          <a href="#" class="nav-icon"
+          <a href="cart.html" class="nav-icon"
             ><img style="width: 25px" src="image/cart.png" alt=""
           /></a>
           <a href="#" class="nav-icon"
@@ -38,19 +46,16 @@ const createNav = () => {
 };
 
 createNav();
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav";
-  }
-}
 
 const loginPage = () => {
-  window.location.href = 'login.html';
-}
+  window.location.href = "login.html";
+};
 
 const productPage = () => {
-  window.location.href = 'product.html';
-}
+  window.location.href = "product.html";
+};
+
+const dropdownbtn = () => {
+  document.getElementById("myDropdown").classList.toggle("show");
+  console.log(document.getElementById("myDropdown").classList.toggle("show"));
+};
